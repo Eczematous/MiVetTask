286e97411e3406f36d50d2c75ffe5ef69998961d
Jonathan Mercado <112668569+Eczematous@users.noreply.github.com>
2022-11-18 15:31:35 -0800

    Add files via upload

---
+++ .NET/Interfaces/ISubscriptionService.cs
@@ -0,0 +1,10 @@
+﻿using Sabio.Models.Domain.Subscriptions;
+using System.Collections.Generic;
+
+namespace Sabio.Services
+{
+    public interface ISubscriptionService
+    {
+        List<Subscription> GetAllByUserId(int userId);
+    }
+}
---
+++ .NET/Interfaces/IVideoChatService.cs
@@ -0,0 +1,19 @@
+﻿using Sabio.Models.Domain;
+using Sabio.Models.Domain.VideoChat;
+using Sabio.Models.Requests.VideoChat;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sabio.Services
+{
+    public interface IVideoChatService
+    {
+        Task<DailyResponse> CreateRoom();
+        Task<DailyRoomListResponse> GetRooms(string room);
+
+        int AddRoom(DailyMeetingAddRequest model);
+        List<DailyMeeting> GetAllRooms();
+
+        List<DailyMeeting> GetRoomsByHostId(int hostId);
+    }
+}
